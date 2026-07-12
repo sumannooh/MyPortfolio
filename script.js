bc4b9174fef8a99a5a2508725a820833c0cb2a84
sumannooh <aiartsuman@gmail.com>
2026-07-12 11:07:19 +0530

    Change password check from '1234' to '22314'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    if (user === "suman" && pass === "1234") {
+    if (user === "suman" && pass === "22314") {
         document.getElementById("message").innerHTML =
         "✅ Welcome Suman!";
     } else {
