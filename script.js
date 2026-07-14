66cdd5445156f8c6a3ef6617d06c794cec70bba9
sumannooh <aiartsuman@gmail.com>
2026-07-14 21:56:14 +0530

    Implement dark mode toggle and scroll button
    Added dark mode toggle and scroll button functionality.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// Dark Mode
 function toggleMode(){
-
-document.body.classList.toggle("dark");
-
+    document.body.classList.toggle("dark");
 }
+
+// Scroll Button
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+    if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        topBtn.style.display = "block";
+    }else{
+        topBtn.style.display = "none";
+    }
+};
+
+function topFunction(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+          }
